fe58c64695d57dff0d5e34c19a15ce354b2ece86
grammar2642 <grammar2642@gmail.com>
2025-03-18 11:41:04 +0900

    timehead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import Clock from "@/components/Clock"
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen">
+    <main className="font-mono">
+      <div className="text-3xl text-center item-center border-3 rounded-full p-4">
       <Clock />
+      </div>      
     </main>
   )
 }
