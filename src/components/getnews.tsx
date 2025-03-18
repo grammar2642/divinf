5f920a73ad8ec49abc51c3528572117003df0021
grammar2642 <grammar2642@gmail.com>
2025-03-18 15:35:51 +0900

    aru

---
+++ src/components/getnews.tsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const NewsList = () => {
+     const[news, setNews] = useState([]);
+     const[loading, setLoading] = useState(true);
+     const API＿KEY = "d65873a8a31842d89ff7a16073560863";
+     const URL = ``
+}
