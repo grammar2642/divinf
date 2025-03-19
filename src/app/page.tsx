ba5585ae452e2dfae4f5b3d4ced0c7658488f92f
grammar2642 <grammar2642@gmail.com>
2025-03-19 14:27:57 +0900

    WeatherApp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 "use client"
 
 import Clock from "@/components/Clock"
+import WeatherApp from "@/pages/weather"
+
 
 export default function Home() {
   return (
     <main className="font-mono">
       <div className="text-3xl text-center item-center border-3 rounded-full p-4">
       <Clock />
-      </div>      
+      </div>
+      <br />      
+      <div className="text-3xl text-center p-10 border-3 rounded-full">
+        <WeatherApp />
+      </div>
     </main>
   )
 }
