1c6d0a53b84509fd79847f54be2d4b0de6025075
grammar2642 <grammar2642@gmail.com>
2025-03-20 16:38:31 +0900

    what

--- src/app/components/weather.tsx
+++ src/app/components/weather.tsx
@@ -1,55 +1,16 @@
-import { useState } from "react";
+"use server";
 
-export default function WeatherApp() {
-     const [city, setCity] = useState<string>("");
-     const [weather, setWeather] = useState<any>(null);
-     const [error, setError] = useState<string | null>(null);
-
-     const getWeather = async () => {
-          if(!city) {
-               setError("Enter your City");
-               return;
-          } 
-          setError(null);
-
-          try {
-               const res = await fetch(`/api/weather?city=${city}`);
-               const data = await res.json();
-
-               if(data.error) {
-                    setError("Failure to get Weather Data");
-               } else {
-                    setWeather(data);
-               }               
-          } catch (err) {
-               setError("Network Error has happened");
-          }
-     };
-
-     return (
-          <div>
-               <h2>WeatherApp</h2>
-               <input
-               type="text"
-               placeholder="Enter your City"
-               value={city}
-               onChange={(e) => setCity(e.target.value)}
-               />
-               <button onClick={getWeather}>
-                    Getting WeatherData
-               </button>
-
-               {error && <p>{error}</p>}
-
-               {weather && weather.main && (
-                    <div>
-                         <p>{weather.name}の天気</p>
-                         <p>気温: {weather.main.temp}℃</p>
-                         <p>湿度: {weather.main.humidity}%</p>
-                         <p>風速: {weather.wind.speed} m/s</p>
-                         <p>天候: {weather.weather[0].description}</p>
-               </div>
-               )}
-          </div>
-     );
+export async function WeatherApp() {
+  const city = "Tokyo";
+  try {
+    console.log("データ取得中です...");
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=ja&appid=${process.env.OPENWEATHER_API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("データ取得エラー");
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 "use client"
 
 import Clock from "./components/Clock"
-import WeatherApp from "./components/weather"
+import { WeatherApp } from "./components/weather"
 
 
 export default function Home() {
   return (
     <main className="font-mono">
-      <div className="text-3xl text-center item-center border-3 rounded-full p-4">
+      <div className="text-3xl text-center item-center border-3 p-4">
       <Clock />
       </div>
       <br />      
-      <div className="text-3xl text-center p-10 border-3 rounded-full">
+      <div className="text-3xl text-center p-10 border-3">
         <WeatherApp />
       </div>
     </main>
