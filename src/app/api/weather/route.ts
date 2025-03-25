531e997145ae8137eaa8e667632e7c280ab585b2
grammar2642 <grammar2642@gmail.com>
2025-03-25 12:46:23 +0900

    Transition

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -1,12 +1,19 @@
-export async function GET() {
-    console.log("API was called");
+import { NextRequest, NextResponse } from "next/server";
 
-    const place = "Osaka";
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const place = searchParams.get("city");
     const API = process.env.WEATHER_API;
 
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&lang=en&appid=${API}`);
+    console.log("API was called");
 
+    const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&lang=en&appid=${API}`
+    );
+    if (!res.ok) {
+        return NextResponse.json({ error: "Cannot approach the city"}, { status: 404});
+    }
     const data = await res.json();
     console.log("取得データ:", data);
-    return Response.json(data);
+    return NextResponse.json(data);
 }
--- src/app/components/weather.tsx
+++ src/app/components/weather.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useTransition } from "react";
 
 export default function WeatherApp() {
 
@@ -11,29 +11,51 @@ export default function WeatherApp() {
     }[];
     main: {
       temp: number;
+      humidity: number;
+      temp_min: number;
+      temp_max: number;
     };
   }
   const [weather, setWeather] = useState<WData | null>();
-const getWeather = async () => {  
-    console.log("データ取得中です...");
-    const response = await fetch("/api/weather");
+  const [isPending, startTransition] = useTransition();
+
+const getWeather = async () => {
+  startTransition(async () => {
+    console.log("Now Loading...");
+    const response = await fetch(`/api/weather?city=${city}`);
     const data = await response.json();
     console.log(data);
     setWeather(data);
     return data;    
+  }) 
+    
   };
-
+  const [city, setCity] = useState("");
+  
   return (
     <>
-    <h1 className="text-4xl">Weather API</h1>
+    
+    <h1 className="text-center items-center flex flex-col text-4xl">Weather API</h1>
+    <input
+    className="text-3xl p-3 mb-5 border-2 rounded-lg"
+    placeholder="Enter city name"
+    type="text"
+    value={city}
+    onChange={(e) =>setCity(e.target.value)}
+    >
+    
+    </input>
     <button 
-    className="text-3xl p-10"
-    onClick={getWeather}>GetWeather</button>
+    className="text-3xl border-2 rounded-lg p-3 mb-5"
+    onClick={getWeather} disabled={isPending}>{isPending ? "Now Loading..." : "Get Weather"}</button>
     {weather && (
       <div className="text-2xl">
         <p>City:{weather.name ?? "不明"}</p>
         <p>Weather:{weather.weather?.[0]?.description ?? "不明"}</p>
         <p>Temperature(Celcius):{weather.main?.temp ?? "不明"}℃</p>
+        <p>humidity:{weather.main.humidity}%</p>
+        <p>Max Temp:{weather.main.temp_max}℃</p>
+        <p>Minimum Temp:{weather.main.temp_min}℃</p>
     </div>
     )}
     </>
